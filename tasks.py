e333e19436d65ee25297d943697706cc22a33739
Miodek <kedoim420@gmail.com>
2022-05-30 10:25:30 +0200

    heroku

---
+++ tasks.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+app = FastAPI()
+@app.get('/start',response_class=HTMLResponse)
+def html_static():
+    return '''
+    <html>
+        <head>
+            
+        </head>
+        <body>
+            <h1>The unix epoch started at 1970-01-01</h1>
+        </body>
+    </html>
+    """
+    '''
+
